04db7feee3a6fc1eccba6ab389cdc2cefd732757
Hajime Hoshi <hajimehoshi@gmail.com>
2019-05-05 00:41:05 +0900

    Add Object interface

--- internal/gamescene/field.go
+++ internal/gamescene/field.go
@@ -16,73 +16,24 @@ package gamescene
 
 import (
 	"image"
-	"image/color"
 	"strings"
 
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
-
-const (
-	tileWidth  = 16
-	tileHeight = 16
-)
-
-type Tile int
-
-const (
-	TileNone Tile = iota
-	TilePart
-	TileOutside
-	TileWallBig
-	TileWallSmall
-	TileFFBig
-	TileFFSmall
-	TileElevator
-	TileStart
-)
-
-func (t Tile) IsBig() bool {
-	switch t {
-	case TileWallBig, TileFFBig:
-		return true
-	}
-	return false
-}
 
 type Field struct {
-	tiles [][]Tile
+	objects []Object
+	startX  int
+	startY  int
 }
 
 func (f *Field) StartPosition() (x, y int) {
-	for j, row := range f.tiles {
-		for i, tile := range row {
-			if tile == TileStart {
-				return i, j
-			}
-		}
-	}
-	panic("gamescene: start position is not found")
-}
-
-func (f *Field) touchingTiles(rect image.Rectangle) []Tile {
-	var tiles []Tile
-	minX := rect.Min.X / tileWidth
-	maxX := rect.Max.X / tileWidth
-	minY := (rect.Min.Y - 1) / tileHeight
-	maxY := (rect.Max.Y - 1) / tileHeight
-	for j := minY; j <= maxY; j++ {
-		for i := minX; i <= maxX; i++ {
-			tiles = append(tiles, f.tileAt(i, j))
-		}
-	}
-	return tiles
+	return f.startX, f.startY
 }
 
 func (f *Field) Conflicts(rect image.Rectangle) bool {
-	for _, t := range f.touchingTiles(rect) {
-		switch t {
-		case TileOutside, TileWallBig, TileWallSmall:
+	for _, t := range f.objects {
+		if t.Conflicts(rect) {
 			return true
 		}
 	}
@@ -90,9 +41,8 @@ func (f *Field) Conflicts(rect image.Rectangle) bool {
 }
 
 func (f *Field) ConflictsWithFoot(rect image.Rectangle) bool {
-	for _, t := range f.touchingTiles(rect) {
-		switch t {
-		case TileOutside, TileWallBig, TileWallSmall, TileElevator:
+	for _, t := range f.objects {
+		if t.ConflictsWithFoot(rect) {
 			return true
 		}
 	}
@@ -100,70 +50,19 @@ func (f *Field) ConflictsWithFoot(rect image.Rectangle) bool {
 }
 
 func (f *Field) InElevator(rect image.Rectangle) bool {
-	for _, t := range f.touchingTiles(rect) {
-		if t == TileElevator {
-			return true
+	for _, t := range f.objects {
+		if _, ok := t.(*ObjectElevator); ok {
+			if t.Touches(rect) {
+				return true
+			}
 		}
 	}
 	return false
 }
 
-func (f *Field) tileAt(x, y int) Tile {
-	if y < 0 || len(f.tiles) <= y {
-		return TileOutside
-	}
-	if x < 0 || len(f.tiles[y]) <= x {
-		return TileOutside
-	}
-	t := f.tiles[y][x]
-	if t != TilePart {
-		return t
-	}
-	if y > 0 {
-		if x > 0 {
-			if t := f.tiles[y-1][x-1]; t.IsBig() {
-				return t
-			}
-		}
-		if t := f.tiles[y-1][x]; t.IsBig() {
-			return t
-		}
-	}
-	if x > 0 {
-		if t := f.tiles[y][x-1]; t.IsBig() {
-			return t
-		}
-	}
-	return t
-}
-
 func (f *Field) Draw(screen *ebiten.Image) {
-	for j, row := range f.tiles {
-		for i, tile := range row {
-			x := i * tileWidth
-			y := j * tileHeight
-			w := 0
-			h := 0
-			if tile.IsBig() {
-				w = tileWidth*2 - 1
-				h = tileHeight*2 - 1
-			} else {
-				w = tileWidth - 1
-				h = tileHeight - 1
-			}
-			switch tile {
-			case TileWallSmall:
-				ebitenutil.DrawRect(screen, float64(x), float64(y), float64(w), float64(h), color.RGBA{0x66, 0x66, 0x66, 0xff})
-			case TileWallBig:
-				ebitenutil.DrawRect(screen, float64(x), float64(y), float64(w), float64(h), color.RGBA{0x66, 0x66, 0x66, 0xff})
-			case TileFFSmall:
-				ebitenutil.DrawRect(screen, float64(x), float64(y), float64(w), float64(h), color.RGBA{0xff, 0x00, 0x00, 0x40})
-			case TileFFBig:
-				ebitenutil.DrawRect(screen, float64(x), float64(y), float64(w), float64(h), color.RGBA{0xff, 0x00, 0x00, 0x40})
-			case TileElevator:
-				ebitenutil.DrawRect(screen, float64(x), float64(y), float64(w), float64(h), color.RGBA{0xff, 0xff, 0x00, 0xff})
-			}
-		}
+	for _, t := range f.objects {
+		t.Draw(screen)
 	}
 }
 
@@ -186,35 +85,27 @@ wwwwwwwwwwwwwwww
 `
 
 func strToField(str string) *Field {
-	var tiles [][]Tile
-	for _, line := range strings.Split(testField, "\n") {
-		if line == "" {
-			continue
-		}
-		var row []Tile
-		for _, c := range line {
+	f := &Field{}
+	for j, line := range strings.Split(strings.TrimSpace(testField), "\n") {
+		for i, c := range line {
 			switch c {
 			case 'W':
-				row = append(row, TileWallBig)
+				f.objects = append(f.objects, &ObjectWall{big: true, x: i, y: j})
 			case 'w':
-				row = append(row, TileWallSmall)
+				f.objects = append(f.objects, &ObjectWall{big: false, x: i, y: j})
 			case 'F':
-				row = append(row, TileFFBig)
+				f.objects = append(f.objects, &ObjectFF{big: true, x: i, y: j})
 			case 'f':
-				row = append(row, TileFFSmall)
+				f.objects = append(f.objects, &ObjectFF{big: false, x: i, y: j})
 			case 'e':
-				row = append(row, TileElevator)
+				f.objects = append(f.objects, &ObjectElevator{x: i, y: j})
 			case 's':
-				row = append(row, TileStart)
+				f.startX = i
+				f.startY = j
 			case '.':
-				row = append(row, TilePart)
 			default:
-				row = append(row, TileNone)
 			}
 		}
-		tiles = append(tiles, row)
 	}
-	return &Field{
-		tiles: tiles,
-	}
+	return f
 }
---
+++ internal/gamescene/object.go
@@ -0,0 +1,141 @@
+// Copyright 2019 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gamescene
+
+import (
+	"image"
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+)
+
+const (
+	tileWidth  = 16
+	tileHeight = 16
+)
+
+type Object interface {
+	Touches(rect image.Rectangle) bool
+	Conflicts(rect image.Rectangle) bool
+	ConflictsWithFoot(rect image.Rectangle) bool
+	Draw(screen *ebiten.Image)
+}
+
+type ObjectWall struct {
+	big bool
+	x   int
+	y   int
+}
+
+func (o *ObjectWall) Touches(rect image.Rectangle) bool {
+	w := tileWidth
+	h := tileHeight
+	if o.big {
+		w *= 2
+		h *= 2
+	}
+	return image.Rect(o.x*tileWidth, o.y*tileHeight, o.x*tileWidth+w, o.y*tileHeight+h).Overlaps(rect)
+}
+
+func (o *ObjectWall) Conflicts(rect image.Rectangle) bool {
+	return o.Touches(rect)
+}
+
+func (o *ObjectWall) ConflictsWithFoot(rect image.Rectangle) bool {
+	return o.Touches(rect)
+}
+
+func (o *ObjectWall) Draw(screen *ebiten.Image) {
+	x := o.x * tileWidth
+	y := o.y * tileHeight
+	if o.big {
+		w := tileWidth*2 - 1
+		h := tileWidth*2 - 1
+		ebitenutil.DrawRect(screen, float64(x), float64(y), float64(w), float64(h), color.RGBA{0x66, 0x66, 0x66, 0xff})
+	} else {
+		w := tileWidth - 1
+		h := tileWidth - 1
+		ebitenutil.DrawRect(screen, float64(x), float64(y), float64(w), float64(h), color.RGBA{0x66, 0x66, 0x66, 0xff})
+	}
+}
+
+type ObjectFF struct {
+	big bool
+	x   int
+	y   int
+
+	on bool
+}
+
+func (o *ObjectFF) Touches(rect image.Rectangle) bool {
+	if !o.on {
+		return false
+	}
+	w := tileWidth
+	h := tileHeight
+	if o.big {
+		w *= 2
+		h *= 2
+	}
+	return image.Rect(o.x*tileWidth, o.y*tileHeight, o.x*tileWidth+w, o.y*tileHeight+h).Overlaps(rect)
+}
+
+func (o *ObjectFF) Conflicts(rect image.Rectangle) bool {
+	return o.Touches(rect)
+}
+
+func (o *ObjectFF) ConflictsWithFoot(rect image.Rectangle) bool {
+	return o.Touches(rect)
+}
+
+func (o *ObjectFF) Draw(screen *ebiten.Image) {
+	x := o.x * tileWidth
+	y := o.y * tileHeight
+	if o.big {
+		w := tileWidth*2 - 1
+		h := tileWidth*2 - 1
+		ebitenutil.DrawRect(screen, float64(x), float64(y), float64(w), float64(h), color.RGBA{0xff, 0x00, 0x00, 0x40})
+	} else {
+		w := tileWidth - 1
+		h := tileWidth - 1
+		ebitenutil.DrawRect(screen, float64(x), float64(y), float64(w), float64(h), color.RGBA{0xff, 0x00, 0x00, 0x40})
+	}
+}
+
+type ObjectElevator struct {
+	x int
+	y int
+}
+
+func (o *ObjectElevator) Touches(rect image.Rectangle) bool {
+	w := tileWidth
+	h := tileHeight
+	return image.Rect(o.x*tileWidth, o.y*tileHeight, o.x*tileWidth+w, o.y*tileHeight+h).Overlaps(rect)
+}
+
+func (o *ObjectElevator) Conflicts(rect image.Rectangle) bool {
+	return false
+}
+
+func (o *ObjectElevator) ConflictsWithFoot(rect image.Rectangle) bool {
+	return o.Touches(rect)
+}
+
+func (o *ObjectElevator) Draw(screen *ebiten.Image) {
+	x := o.x * tileWidth
+	y := o.y * tileHeight
+	ebitenutil.DrawRect(screen, float64(x), float64(y), float64(tileWidth-1), float64(tileHeight-1), color.RGBA{0xff, 0xff, 0x00, 0xff})
+}
